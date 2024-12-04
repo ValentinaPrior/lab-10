695a14d6fe59d2640303b5e97de209554d54cc52
ValentinaPrior <vbp7@njit.edu>
2024-12-03 22:50:17 -0500

    4-6

--- lab.js
+++ lab.js
@@ -19,8 +19,31 @@ function addNumbers()
     var num1= parseFloat(document.getElementById("number1").value);
     var num2= parseFloat(document.getElementById("number2").value);
     var result= num1+ num2;
-    document.getElementById("result=").innerText= "Result "+result};
+    document.getElementById("result=").innerText= "Result "+result;
+}
     
+//Question four
+function updateTitle()
+{
+    var newTitle= document.getElementById("inputField").value;
+    document.getElementById("title").textContent=newTitle;
+}
+
+//Question five
+function combineText() {
+  
+    var text1 = document.getElementById("inputOne").value;
+    var text2 = document.getElementById("inputTwo").value;
+    var combined = text1 + " " + text2;
+    document.getElementById("combinedText").innerText = combined;
+}
+
+//Question six
+const button=document.getElementById('changeButton');
+const  colorBox= document.getElementById('colorBox');
+button.addEventListener('click', function() {
+    colorBox.style.backgroundColor='blue';
+})
 
 
 
