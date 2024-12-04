baf291fb0226e5cabce8a70a49f8bd7bb61facb9
ValentinaPrior <vbp7@njit.edu>
2024-12-03 22:19:57 -0500

    question one-three

---
+++ lab.js
@@ -0,0 +1,27 @@
+//Question one
+function greetUser(username)
+{
+    let username=document.getElementById('usrname').value;
+    document.getElementById('output').innerText="hello, "+username;
+
+    div.innerHTML= `Hello, ${name}! Welcome to the site.`;
+}
+
+//Question two
+function changeText()
+{
+    document.getElementById("message").innerText= "Hello, World!";
+}
+
+//Question three
+function addNumbers()
+{
+    var num1= parseFloat(document.getElementById("number1").value);
+    var num2= parseFloat(document.getElementById("number2").value);
+    var result= num1+ num2;
+    document.getElementById("result=").innerText= "Result "+result};
+    
+
+
+
+
